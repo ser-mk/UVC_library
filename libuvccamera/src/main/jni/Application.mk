7c6d57d616e13f59ee43817c6ae48dd2984543b0
Evgeniy Chormonov <echormonov@smartlabs.tv>
2017-12-03 22:28:29 +0300

    CUSTOM: change on x86 build

--- libuvccamera/src/main/jni/Application.mk
+++ libuvccamera/src/main/jni/Application.mk
@@ -27,8 +27,8 @@
 #NDK_TOOLCHAIN_VERSION := 4.9
 
 APP_PLATFORM := android-14
-APP_ABI := arm64-v8a
-#armeabi armeabi-v7a
+APP_ABI := x86
+#armeabi armeabi-v7a arm64-v8a
 # x86 mips
 #APP_OPTIM := debug
 APP_OPTIM := release
